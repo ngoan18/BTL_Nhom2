c47ae94d939ad73a0acdbe5e4ff7f058e5b3d88c
hattiu <hattiu210817@gmail.com>
2023-10-03 08:56:38 +0700

    Table

--- Controllers/LuongController.cs
+++ Controllers/LuongController.cs
@@ -159,5 +159,20 @@ namespace qlnv.Controllers
         {
           return (_context.Luong?.Any(e => e.LuongCB == id)).GetValueOrDefault();
         }
+
+        
+        [HttpPost]
+        public IActionResult Index(string luongCB, string heSo, string phuCap)
+        {
+            double cb =0, hs =0, pc =0, luong = 0;
+            string luongNV;
+            if(!String.IsNullOrEmpty(luongCB)) cb = Convert.ToDouble(luongCB);
+            if(!String.IsNullOrEmpty(heSo)) hs = Convert.ToDouble(heSo);
+            if(!String.IsNullOrEmpty(phuCap)) pc = Convert.ToDouble(phuCap);
+            luong = cb*hs+pc;
+            luongNV = "Lương : " + luong ;
+            ViewBag.luong = luongNV;
+            return View();
+        }
     }
 }
